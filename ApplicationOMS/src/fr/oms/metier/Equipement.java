2150da0257d90f81b64ef44db65356fd307e271e
Kévin <kevin.malleret03@gmail.com>
2015-02-24 15:14:03 +0100

    Création de la classe Equipement

---
+++ ApplicationOMS/src/fr/oms/metier/Equipement.java
@@ -0,0 +1,50 @@
+package fr.oms.metier;
+
+public class Equipement {
+
+	private int uid;
+	private String nom;
+	private String adresse;
+	private Quartier quartier;
+	
+	public Equipement(int unId, String unNom, String uneAdresse, Quartier unQuartier){
+		uid = unId;
+		nom = unNom;
+		adresse = uneAdresse;
+		quartier = unQuartier;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public Quartier getQuartier() {
+		return quartier;
+	}
+
+	public void setQuartier(Quartier quartier) {
+		this.quartier = quartier;
+	}
+	
+	
+}
