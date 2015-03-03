8130225a8115a29893c854f4898bf79cf61379e3
Kévin <kevin.malleret03@gmail.com>
2015-03-03 15:06:01 +0100

    Creation d'une boite de dialogue pour afficher la liste des sports pour une discipline (plus efficace et plus fiable que le spinner)

--- ApplicationOMS/src/fr/oms/activities/ListDisciplineActivity.java
+++ ApplicationOMS/src/fr/oms/activities/ListDisciplineActivity.java
@@ -9,13 +9,14 @@ import fr.oms.metier.Discipline;
 import fr.oms.metier.Sport;
 import fr.oms.modele.Manager;
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ListDisciplineActivity extends Activity {
@@ -36,12 +37,40 @@ public class ListDisciplineActivity extends Activity {
 		donneSport();
 	}
 	
-	public void donneSport(){
-		listeDiscipline.setOnItemClickListener(new ListView.OnItemClickListener() {
-
+	private void donneSport(){
+		listeDiscipline.setOnItemClickListener(new ListView.OnItemClickListener(){
+			private ListView listeSport;
+			private Discipline discipline;
+			
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				
+				Dialog dialog=new Dialog(ListDisciplineActivity.this);
+				dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+				dialog.setContentView(R.layout.dialog_sport);
+				TextView titreDialog = (TextView)dialog.findViewById(R.id.titreDialogSport);
+				listeSport = (ListView)dialog.findViewById(R.id.listeSport);
+			    discipline = Manager.getInstance().getListeDiscipline().get(position);
+				titreDialog.setText(discipline.getNom() + " : ");
+				List<String> mesNomsSports = new ArrayList<String>();
+				for(Sport sport : discipline.getListeSport()){
+					mesNomsSports.add(sport.getNom());
+				}
+				ArrayAdapter<String> sportAdapter = new ArrayAdapter<String>(ListDisciplineActivity.this, android.R.layout.simple_list_item_1, mesNomsSports);
+				listeSport.setAdapter(sportAdapter);
+				clicSurUnSport();
+				dialog.show();
+			}
+			
+			private void clicSurUnSport(){
+				listeSport.setOnItemClickListener(new ListView.OnItemClickListener(){
+
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+						Sport sport = discipline.getListeSport().get(position);
+						Toast.makeText(ListDisciplineActivity.this, sport.getNom(), Toast.LENGTH_SHORT).show();
+					}
+					
+				});
 			}
 		});
 	}
--- ApplicationOMS/src/fr/oms/adapter/DisciplineAdapter.java
+++ ApplicationOMS/src/fr/oms/adapter/DisciplineAdapter.java
@@ -9,10 +9,13 @@ import fr.oms.metier.Sport;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,12 +23,10 @@ import android.widget.Toast;
 public class DisciplineAdapter extends ArrayAdapter<Discipline> {
 
 	public Context mContext;
-	private int cpt;
 	
 	public DisciplineAdapter(Context context, int resource, List<Discipline> objects) {
 		super(context, resource, objects);
 		mContext = context;
-		cpt = 0;
 	}
 	
 	@Override
@@ -34,34 +35,6 @@ public class DisciplineAdapter extends ArrayAdapter<Discipline> {
 		final Discipline discipline = getItem(position);
 		TextView nomDiscipline = (TextView) convertView.findViewById(R.id.nom_element);
 		nomDiscipline.setText(discipline.getNom());
-		Spinner spinnerSport = (Spinner)convertView.findViewById(R.id.spinner);
-		List<Sport> sports = discipline.getListeSport();
-		List<String> mesNomsSports = new ArrayList<String>();
-		for(Sport sport : sports){
-			mesNomsSports.add(sport.getNom());
-		}
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_spinner_item, mesNomsSports);
-		spinnerSport.setAdapter(adapter);
-		spinnerSport.setSelected(false);
-		spinnerSport.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-			@Override
-			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-				//Fonctionne mais c'est moche
-				if(cpt > discipline.getListeSport().size()){
-					Sport sport = discipline.getListeSport().get(position);
-					Toast.makeText(mContext, sport.getNom(), Toast.LENGTH_SHORT).show();
-				}
-				cpt++;
-				
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> parent) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
 		return convertView;
 	}
 
